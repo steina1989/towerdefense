26c5d9c875bced1edb3d5a92270b9d337c9cc947
ragnaolafs <rao9@hi.is>
2017-11-07 14:48:00 +0000

    Added functions in balloon.js and added Balloon.js to index.html

--- Balloon.js
+++ Balloon.js
@@ -10,27 +10,58 @@
 0        1         2         3         4         5         6         7         8
 12345678901234567890123456789012345678901234567890123456789012345678901234567890
 */
-/*
-function Balloon(descr){
-	this.setup(descr);
+
+//function Balloon(descr){
+function Balloon(){
+	//this.setup(descr);
       
     // Default sprite and scale, if not otherwise specified
-    this.sprite = this.sprite || g_sprites.rock;
-    this.scale  = this.scale  || 1;
-}*/
+    //this.sprite = this.sprite || g_sprites.rock;
+    //this.scale  = this.scale  || 1;
+    this.sprite = g_sprites.balloon;
+    this._scale = 1;
+};
 
-Balloon.prototype.findDirection(){
+Balloon.prototype=new Entity();
+
+Balloon.prototype.firstCell = Arena.getIndexOfCellNumber(1);
+Balloon.prototype.cx = Arena.indexToPos(firstCell).x;
+Balloon.prototype.cy = Arena.indexToPos(firstCell).y;
+//Balloon.prototype.velX = ...
+//Balloon.prototype.velY = ...
+
+Balloon.prototype.update = function(du) {
+
+};
+
+Balloon.prototype.render = function(ctx) {
+	// temporary drawing only in cell no. 1
+	var currentCell = Arena.getIndexOfCellNumber(1);
+	var pos = Arena.indexToPos(currentCell);
+
+	//this.cx = pos.x;
+	//this.cy = pos.y
+
+	var origScale = this.sprite.scale;
+    // draw scaled sprite
+    this.sprite.scale = this._scale;
+    this.sprite.drawWrappedCentredAt(
+	ctx, this.cx, this.cy, this.rotation
+    );
+    this.sprite.scale = origScale;
+};
+
+Balloon.prototype.findDirection = function(){
 	this.nextX = Arena.nextCellInPath.nextY(this.cx,this.cy);
 	this.nextY = Arena.nextCellInPath.nextX(this.cx.this.cy);
-}
+};
 
-Balloon.prototype.takeBulletHit(){
+Balloon.prototype.takeBulletHit = function(){
 	this.isDeadNow = true;
 	//play sound?
 
-}
+};
 
-Balloon.prototype=new Entity();
 
 var redBloon = new Balloon({
 	cx: 50,
